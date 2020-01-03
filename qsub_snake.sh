8407b02e0e284ff6a7db272da1c67f954afc3ec1
Liting Chen <lchen@n0082.compute.hpc>
2020-01-03 19:26:58 +0100

    change all threads to 1 . Later on if no problem, gradually introduce threads.

--- qsub_snake.sh
+++ qsub_snake.sh
@@ -1,3 +1,3 @@
 # 20200103
 # configfile: config_test.yaml
-snakemake --jobs 4 --latency-wait 60 --use-conda --cluster "qsub -o /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stdout.txt -e /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stderr.txt -pe threaded {threads}"
+snakemake --jobs 1 --latency-wait 60 --use-conda --cluster "qsub -o /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stdout.txt -e /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stderr.txt -pe threaded 1"
