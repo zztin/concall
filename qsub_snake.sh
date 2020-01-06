8061839691a1b47050bfa6a644e3c7e3edd4c9ef
Liting Chen <lchen@hpcs03.op.umcutrecht.nl>
2020-01-06 18:43:38 +0100

    add tmp space. working.

--- qsub_snake.sh
+++ qsub_snake.sh
@@ -1,3 +1,3 @@
 # 20200103
 # configfile: config_test.yaml
-snakemake --jobs 8 --latency-wait 120 --use-conda --verbose --cluster "qsub -o /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stdout.txt -e /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stderr.txt -pe threaded {threads}"
+snakemake --jobs 8 --latency-wait 120 --use-conda --verbose --cluster "qsub -l h_rt=00:30:00 -l h_vmem=30G -l tmpspace=40G -o /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stdout.txt -e /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stderr.txt -pe threaded {threads}"
