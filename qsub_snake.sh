c24ae5a21df520833aa1a5038ebf63b1ffac46a6
Liting Chen <lchen@n0046.compute.hpc>
2020-01-06 16:55:45 +0100

    Successful. qsub rules separately (No group jobs).

--- qsub_snake.sh
+++ qsub_snake.sh
@@ -1,3 +1,3 @@
 # 20200103
 # configfile: config_test.yaml
-snakemake --jobs 1 --latency-wait 60 --use-conda --cluster "qsub -o /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stdout.txt -e /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stderr.txt -pe threaded 1"
+snakemake --jobs 8 --latency-wait 120 --use-conda --verbose --cluster "qsub -o /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stdout.txt -e /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stderr.txt -pe threaded {threads}"
