0c66110c0a034f38de03d7f1179263ce5d57cbc4
Liting Chen <lchen@hpcs03.op.umcutrecht.nl>
2020-01-07 19:26:29 +0100

    1. change bb4 into single bb, reduce redundant matching. 2. qsub increase gate-time since bowtie match took longer than 4 hours. 3. sam2.py allow change params for long insert  matching.

--- qsub_snake.sh
+++ qsub_snake.sh
@@ -1,3 +1,3 @@
 # 20200103
 # configfile: config_test.yaml
-snakemake --jobs 8 --latency-wait 120 --use-conda --cluster "qsub -l h_rt=04:00:00 -l h_vmem=40G -l tmpspace=100G -o /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stdout.txt -e /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stderr.txt -pe threaded {threads}"
+snakemake --jobs 50 --latency-wait 120 --use-conda --rerun-incomplete --cluster "qsub -l h_rt=24:00:00 -l h_vmem=40G -l tmpspace=100G -o /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stdout.txt -e /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stderr.txt -pe threaded {threads}"
