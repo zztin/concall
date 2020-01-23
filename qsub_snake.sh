31c4314fe8ef6efc1afda149ab63c2b745573a7c
Liting Chen <lchen@n0061.compute.hpc>
2020-01-23 11:43:52 +0100

    re-run wg. if failed, role back.

--- qsub_snake.sh
+++ qsub_snake.sh
@@ -1,3 +1,3 @@
 # 20200103
 # configfile: config_test.yaml
-snakemake --jobs 50 --latency-wait 300 --use-conda --rerun-incomplete --cluster "qsub -l h_rt=32:00:00 -l h_vmem=40G -l tmpspace=100G -o /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stdout.txt -e /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stderr.txt -pe threaded {threads}"
+snakemake --jobs 50 --latency-wait 120 --use-conda --rerun-incomplete --cluster "qsub -l h_rt=4:00:00 -l h_vmem=60G -l tmpspace=150G -o /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stdout.txt -e /hpc/cog_bioinf/ridder/users/lchen/Projects/Medaka_t/concall/log/$(date +"%Y%m%d-%H%M%S")snake_stderr.txt -pe threaded {threads}"
