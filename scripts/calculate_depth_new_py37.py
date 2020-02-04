3d28e1754ffda40cdabbbbe61c96f30e0ec10e56
Liting Chen <lchen@n0082.compute.hpc>
2020-02-04 14:42:54 +0100

    add size selection for good backbone matches.

--- scripts/calculate_depth_new_py37.py
+++ scripts/calculate_depth_new_py37.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
         group = OptionGroup(parser, "Main options")
         group.add_option("-k", dest="target_key", metavar="[PATH]", help="target_key [e.g. TP53]")
         group.add_option("-v", dest="target_value", metavar="[PATH]", help="value_key [e.g. 17:7565097-7590856]")
-        group.add_option("-r", default="no", dest="cosmic", help="calculate COSMIC position TP53 yes/no [default= yes]")
+        group.add_option("-r", default="yes", dest="cosmic", help="calculate COSMIC position TP53 yes/no [default= yes]")
         group.add_option("-i", default=os.getcwd(), dest="input_dir", help="assign directory to find sorted.bam files, if not given, default is cwd")
-        group.add_option("-o", default=os.getcwd(), dest="output", help="output directory")
+        group.add_option("-o", default=None, dest="output", help="output directory")
         parser.add_option_group(group)
         (opt, args) = parser.parse_args()
 
@@ -21,9 +21,9 @@ else:
     dic={"exon12":"17:7577000-7577180","BB22":"BB22:1-248","BB24":"BB24:1-248","BB25":"BB25:1-248","BBCR":"BB22:1-248","TP53":"17:7565097-7590856","PJET":"PJET:1-2974","EGFR":"7:55081714-55329313"}
 
 sambamba="/hpc/local/CentOS7/cog/software/sambamba-0.6.5/sambamba"
-cwd = os.getcwd()
 cosmic= "/hpc/cog_bioinf/ridder/tools/Cyclomics_consensus_pipeline/data_files/COSMIC_mutations.bed"
-
+if opt.output == None:
+    opt.output= opt.input_dir
 for f in os.listdir(opt.input_dir):
     if f.endswith((".sorted.bam")):
         for item in dic:
@@ -40,8 +40,8 @@ for f in os.listdir(opt.input_dir):
 # Create folders
 move = True
 if move ==True:
-    os.system("mkdir 03_sam 04_bam 05_sorted 06_sambamba")
-    os.system("mv *.sorted.* 05_sorted")
-    os.system("mv *.sam 03_sam")
-    os.system("mv *.bam 04_bam")
-    os.system("mv *sambamba_out* 06_sambamba")
+    os.system(f"mkdir -p {opt.output}/01_sam {opt.output}/02_bam {opt.output}/05_sorted {opt.output}/06_sambamba")
+    os.system(f"mv {opt.output}/*.sorted.* {opt.output}/03_sorted")
+    os.system(f"mv {opt.output}/*.sam {opt.output}/01_sam")
+    os.system(f"mv {opt.output}/*.bam {opt.output}/02_bam")
+    os.system("mv {opt.output}/*sambamba_out* {opt.output}/04_sambamba")
