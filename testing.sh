c3da071781dfab2692e16ff29dad48a78fbf28f6
Liting Chen <l.t.chen-4@umcutrecht.nl>
2020-03-24 16:53:19 +0100

    add testing data

--- testing.sh
+++ testing.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 TITLE=$1
-snakemake --cluster sge_wrapper.py --jobs 50 --latency-wait 240 --use-conda --use-singularity --rerun-incomplete --keep-going --restart-times 3 --configfile ./test/config/config-$TITLE.yaml
-snakemake --report $(date +"%Y%m%d-%H%M%S")-$TITLE.html --configfile ./test/config/config-$TITLE.yaml
+snakemake --cluster sge_wrapper.py --jobs 50 --latency-wait 240 --use-conda --use-singularity --rerun-incomplete --keep-going --restart-times 3 --configfile ./configfiles/config-$TITLE.yaml
+snakemake --report $(date +"%Y%m%d-%H%M%S")-$TITLE.html --configfile ./configfiles/config-$TITLE.yaml
