5f7b224e25af1dee3ec80cf9df9a3040f6b8591e
Liting Chen <l.t.chen-4@umcutrecht.nl>
2020-03-24 11:34:17 +0100

    update bash submission to configfiles folder path

--- testing.sh
+++ testing.sh
@@ -1,3 +1,4 @@
 #/bin/bash
-snakemake --restart-times 3 --configfile config_test2.yaml  --use-singularity
-#snakemake --configfile config_test2.yaml --report
+TITLE=$1
+snakemake --cluster sge_wrapper.py --jobs 50 --latency-wait 240 --use-conda --use-singularity --rerun-incomplete --keep-going --restart-times 3 --configfile ./test/config/config-$TITLE.yaml
+snakemake --report $(date +"%Y%m%d-%H%M%S")-$TITLE.html --configfile ./test/config/config-test2.yaml
