0c51ad27f9ea9eae49f9b4140524acd33f5f0ee2
Liting Chen <l.t.chen-4@umcutrecht.nl>
2020-03-25 12:49:16 +0100

    add test unzip fastq file. successful locally.

--- test_multi.sh
+++ test_multi.sh
@@ -1,5 +1,16 @@
-#5d55c11c10cd48aeba184be1301af2dfcb49ede4 --  test2 always work with this version
-./testing.sh test2
+# first qlogin 
+conda activate snake-mdk
+snakemake --use-conda --use-singularity --configfiles ./configfiles/config-test40unzip.yaml
 echo $?
 if [ "$?" == 0 ];
-    echo "qsub testing test2 done. successful"
+    echo "local test40unzip  done. successful"
+sh ./qsub_snakemake.sh test40unzip
+echo $?
+if [ "$?" == 0 ];
+    echo "sh qsub_snakemake.sh test40unzip done. successful"
+
+#5d55c11c10cd48aeba184be1301af2dfcb49ede4 --  test2 always work with this version
+sh ./qsub_snakemake.sh test2
+echo $?
+if [ "$?" == 0 ];
+    echo "sh qsub_snakemake.sh test40gz done. successful"
