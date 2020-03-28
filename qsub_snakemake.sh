9ea491618fe5b646d9b15758f869fd0ca6d7b3af
Liting Chen <l.t.chen-4@umcutrecht.nl>
2020-03-28 13:21:33 +0100

    change report paths. disable --quiet tag. snakemake log can be found at .snakemake/log/datetime-...

--- qsub_snakemake.sh
+++ qsub_snakemake.sh
@@ -1,4 +1,4 @@
 TITLE=$1
-snakemake --cluster sge_wrapper.py --jobs 50 --latency-wait 240 --use-conda --use-singularity --rerun-incomplete --keep-going --restart-times 3 --quiet --configfile ./configfiles/config-$TITLE.yaml
+snakemake --cluster sge_wrapper.py --jobs 100 --latency-wait 240 --use-conda --use-singularity --rerun-incomplete --keep-going --restart-times 3  --configfile ./configfiles/config-$TITLE.yaml
 snakemake --report ./reports/$(date +"%Y%m%d-%H%M%S")-$TITLE.html --configfile ./configfiles/config-$TITLE.yaml
-echo "report generated at ./datetime+title.html"
+echo "report generated at ./reports/datetime+title.html"
