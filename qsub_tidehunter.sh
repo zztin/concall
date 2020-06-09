8c06796b6886853ca8637279299277b646007a81
Liting Chen <l.t.chen-4@umcutrecht.nl>
2020-06-09 23:18:42 +0200

    qsub_tidehunter.sh works with TEST40

--- qsub_tidehunter.sh
+++ qsub_tidehunter.sh
@@ -1,3 +1,3 @@
 TITLE=$1
 snakemake --cluster "qsub -V -b y -S /bin/bash" --jobs 50 --latency-wait 240 --use-conda --rerun-incomplete --keep-going --configfile ./configfiles/config-$TITLE.yaml --snakefile ./tidehunter_only.smk
-#snakemake --snakefile ./tidehunter_only.smk --configfile ./configfiles/config-$TITLE.yaml --report ./reports/$(date +"%Y%m%d-%H%M%S")-$TITLE.html
+snakemake --snakefile ./tidehunter_only.smk --configfile ./configfiles/config-$TITLE.yaml --report ./reports/$(date +"%Y%m%d-%H%M%S")-$TITLE.html
